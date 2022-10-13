d9c846a75be6ff61a74f3814b9095ee467ffb238
Karl Heitmann <khf1988@gmail.com>
2022-10-13 18:07:06 -0300

    Initial commit: does a basic search and show results

---
+++ main.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/ruby
+# frozen_string_literal: true
+
+require 'json'
+
+class DataMatch
+  def initialize(data)
+    @data = data
+  end
+
+  def to_s
+    "#{@data}"
+  end
+end
+
+class Begin < DataMatch
+  def to_s
+    "#{@data}\n\t#{@data.keys}"
+    "\tpath: #{@data['path']}"
+  end
+end
+
+class Match < DataMatch
+  def to_s
+    s = <<-STRING
+    \tpath: #{@data['path']}
+    \tlines: #{@data['lines']}
+    \tline_number: #{@data['line_number']}
+    \tabsolute_offset: #{@data['absolute_offset']}
+    \tsubmatches: #{@data['submatches']}
+    STRING
+    "#{@data}\n\t#{@data.keys}"
+    s
+  end
+end
+
+class End < DataMatch
+  def to_s
+    s = <<-STRING
+    \tpath: #{@data['path']}
+    \tbinary_offset: #{@data['binary_offset']}
+    \tstats: #{@data['stats']}
+    STRING
+    s
+  end
+end
+
+class Summary < DataMatch
+  def to_s
+    s = <<-STRING
+    \telapsed_total: #{@data['elapsed_total']}
+    \tstats: #{@data['stats']}
+    STRING
+    "#{@data}\n\t#{@data.keys}"
+    s
+  end
+end
+
+
+# Node models a match from rg --json
+class Node
+  def initialize(match)
+    d = JSON.parse(match)
+    t = d['type']
+    klass = dataKlass(t, d['data'])
+    @raw_data = klass.new(d['data'])
+  end
+
+  def dataKlass(t, d)
+    if t == 'begin'
+      Begin
+    elsif t == 'match'
+      Match
+    elsif t == 'end'
+      End
+    elsif t == 'summary'
+      Summary
+    else
+      puts t
+      1/0
+    end
+  end
+
+  def self.parse(match)
+    new(match)
+  end
+
+  def to_s
+    "type: #{@raw_data.class}\n#{@raw_data}\n\n"
+  end
+end
+
+def run(lines)
+  matches = lines.map { Node.parse(_1) }
+
+  matches.each do |match|
+    puts match
+  end
+end
+
+run(ARGF.readlines)
