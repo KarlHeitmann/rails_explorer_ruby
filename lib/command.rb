21a49a180d695ab3afb4870c50bb50f9b5f801fa
Karl Heitmann <khf1988@gmail.com>
2022-11-09 22:52:25 -0300

    rubocop on lib/command.rb

--- lib/command.rb
+++ lib/command.rb
@@ -1,72 +1,79 @@
+# frozen_string_literal: true
+
 require 'tty-option'
 class Command
   include TTY::Option
 
   usage do
-    program "rails_explorer"
+    program 'rails_explorer'
 
-    command "run"
+    command 'run'
 
-    desc "Run a command in a new container"
+    desc 'Run a command in a new container'
 
-    example "Set working directory (-w)",
-            "  $ dock run -w /path/to/dir/ ubuntu pwd"
+    example 'Set working directory (-w)',
+            '  $ dock run -w /path/to/dir/ ubuntu pwd'
 
-    example <<~EOS
-    Mount volume
-      $ dock run -v `pwd`:`pwd` -w `pwd` ubuntu pwd
-    EOS
+    example <<~EXAMPLE
+      Mount volume
+        $ dock run -v `pwd`:`pwd` -w `pwd` ubuntu pwd
+    EXAMPLE
   end
 
   argument :search_term do
     required
-    desc "Search term"
+    desc 'Search term'
   end
 
   argument :path do
     optional
-    desc "Search term"
+    desc 'Search term'
   end
 
   argument :command do
     optional
-    desc "The command to run inside the image"
+    desc 'The command to run inside the image'
+  end
+
+  keyword :prefix do
+    default ''
+    desc 'Prefix to erase on files window'
   end
 
   keyword :restart do
-    default "no"
+    default 'no'
     permit %w[no on-failure always unless-stopped]
-    desc "Restart policy to apply when a container exits"
+    desc 'Restart policy to apply when a container exits'
   end
 
   flag :quick do
-    short "-q"
-    long "--quick"
-    desc "Quick run"
+    short '-q'
+    long '--quick'
+    desc 'Quick run'
   end
 
   flag :help do
-    short "-h"
-    long "--help"
-    desc "Print usage"
+    short '-h'
+    long '--help'
+    desc 'Print usage'
   end
 
   flag :autopilot do
-    short "-a"
-    long "--autopilot"
-    desc "Starts in autopilot"
+    short '-a'
+    long '--autopilot'
+    desc 'Starts in autopilot'
   end
 
   option :auto do
     required
-    long "--auto string"
-    desc "Assign a name to the container"
+    long '--auto string'
+    desc 'Assign a name to the container'
   end
 
   option :port do
     arity one_or_more
-    short "-p"
-    long "--publish list"
+    short '-p'
+    long '--publish list'
     convert :list
     desc "Publish a container's port(s) to the host"
   end
