df76ff41a52793e24556fe105f280046399baf90
Karl Heitmann <khf1988@gmail.com>
2022-11-11 00:33:13 -0300

    fix: when special found has a match, it shows the matches of the file AND shows the  option to do another search

--- lib/explorer.rb
+++ lib/explorer.rb
@@ -88,16 +88,16 @@ module Explorer
       file_name = complete_file_name.split('/')[-1]
       return unless (file_name[0] == '_') && (file_name[-3..] == 'erb')
 
-      clear_screen
+      plugin_rails_command = "render.*#{file_name.split('.').first[1..]}" # TODO: Here we need to apply a plugin
       choices = [
-        { name: file_name, value: 1 },
-        { name: 'Quit', value: 'q' },
+        { name: "Spawn explorer to search >>\"#{plugin_rails_command}\"<<", value: 1 },
+        { name: 'Return', value: 'q' },
       ]
       option = @prompt.enum_select("====> Select an option for #{complete_file_name}", choices)  
       return if option == 'q'
 
       explorer_child_data = @explorer_data
-      explorer_child_data[:search_term] = "render.*#{file_name.split('.').first[1..]}"
+      explorer_child_data[:search_term] = plugin_rails_command
       explorer_child = Nodes.new(explorer_data: explorer_child_data)
       explorer_child.menu
     end
